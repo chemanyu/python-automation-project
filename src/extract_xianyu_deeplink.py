907c80c721a704c5a900bcbbd74b447854b92bf2
chemanyu-os <chemanyu-os@360os.com>
2026-01-15 15:54:20 +0800

    fix

--- src/extract_xianyu_deeplink.py
+++ src/extract_xianyu_deeplink.py
@@ -12,7 +12,7 @@ from urllib.parse import urlparse
 
 # 配置 ChromeDriver 路径
 #CHROME_DRIVER_PATH = "/opt/homebrew/bin/chromedriver"
-CHROME_DRIVER_PATH = "D:\\142\\chromedriver-win64\\chromedriver.exe"
+CHROME_DRIVER_PATH = "D:\\144\\chromedriver-win64\\chromedriver.exe"
 
 def get_xianyu_deeplink(short_url, driver=None, platform="android"):
     """
